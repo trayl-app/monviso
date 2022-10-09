9867f68b0f6bd2c04d4b0d642f7f1358cb4eb765
ocintnaf <ocintnaf@gmail.com>
2022-10-09 10:44:29 +0200

    Initial commit

---
+++ test/jest-e2e.config.ts
@@ -0,0 +1,9 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
