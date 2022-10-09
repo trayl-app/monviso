b0a0a87f6232ed4cabf41e6501ec2beb14214c1f
ocintnaf <ocintnaf@gmail.com>
2022-10-09 11:52:36 +0200

    install prisma and @prisma/client. create basic controllers for users and profiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({ imports: [UsersModule, ProfilesModule] })
 export class AppModule {}
