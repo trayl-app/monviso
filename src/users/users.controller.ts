3721b7c7a7c7c4f28edf0896926a0a14192fc7a5
ocintnaf <ocintnaf@gmail.com>
2022-10-12 20:20:13 +0200

    test, temporary endpoint for testing purposes (#12)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('v1/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User> {
-    return this.usersService.findOne(id);
+  /**
+   * Test endpoint
+   *
+   * POST /v1/users
+   * @param {CreateUserDto} createUserDto - The user to create
+   * @returns {Promise<UserEntity>} The created user
+   */
+  @Post()
+  async test(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+    const createdUser = await this.usersService.test(createUserDto);
+
+    return new UserEntity(createdUser);
   }
 }
