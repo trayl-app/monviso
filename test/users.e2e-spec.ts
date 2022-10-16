71c4b1d4698bb922b03362509f4e56015d1dd2ed
ocintnaf <ocintnaf@gmail.com>
2022-10-16 17:49:53 +0200

    fix/enhance tests (#24)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,19 +4,29 @@ import { TestingModule, Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { FirebaseService } from '../src/firebase/firebase.service';
 import { createUserDtoFixture } from '../src/users/fixtures/create-user.dto';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('/api/v1/users', () => {
   let app: INestApplication;
+  let firebaseService: FirebaseService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
       .overrideProvider(FirebaseService)
-      .useValue({})
+      .useValue({
+        auth: {
+          verifyIdToken: jest.fn(),
+        },
+      })
       .compile();
 
     app = moduleFixture.createNestApplication();
+    firebaseService = moduleFixture.get<FirebaseService>(FirebaseService);
+    prismaService = moduleFixture.get<PrismaService>(PrismaService);
+
     await app.init();
   });
 
@@ -25,18 +35,53 @@ describe('/api/v1/users', () => {
   });
 
   describe('POST', () => {
-    it('201', async () => {
-      const createUserDto = createUserDtoFixture();
+    describe('Success', () => {
+      it('201', async () => {
+        const createUserDto = createUserDtoFixture();
 
-      const res = await request(app.getHttpServer())
-        .post('/v1/users')
-        .send(createUserDto);
+        (firebaseService.auth.verifyIdToken as jest.Mock).mockResolvedValue({
+          user_id: createUserDto.id,
+        });
 
-      expect(res.status).toEqual(201);
-      expect(res.body).toEqual({
-        ...createUserDto,
-        createdAt: expect.any(String),
-        updatedAt: expect.any(String),
+        const res = await request(app.getHttpServer())
+          .post('/v1/users')
+          .send(createUserDto)
+          .set('Authorization', 'Bearer token')
+          .expect(201);
+
+        expect(res.body).toEqual({
+          ...createUserDto,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        });
+      });
+    });
+
+    describe('Failure', () => {
+      it('401', async () => {
+        (firebaseService.auth.verifyIdToken as jest.Mock).mockRejectedValue(
+          new Error('error'),
+        );
+
+        await request(app.getHttpServer()).post('/v1/users').send().expect(401);
+      });
+
+      it('409', async () => {
+        const createUserDto = createUserDtoFixture();
+
+        await prismaService.user.create({
+          data: createUserDto,
+        });
+
+        (firebaseService.auth.verifyIdToken as jest.Mock).mockResolvedValue({
+          user_id: createUserDto.id,
+        });
+
+        await request(app.getHttpServer())
+          .post('/v1/users')
+          .send(createUserDto)
+          .set('Authorization', 'Bearer token')
+          .expect(409);
       });
     });
   });
